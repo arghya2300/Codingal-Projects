0e82307f9a9a16e65ba6ba6ec1e2cd92c298594c
arghya2300 <arghya.sarkar.bd@gmail.com>
2025-04-13 19:57:50 +0100

    ArrayReverse

---
+++ Recursion Project/ArrayReverse.py
@@ -0,0 +1,9 @@
+def reverse_array(arr):
+    return arr[::-1]
+
+original_array = [6, 5, 4, 3, 2, 1]
+
+reversed_array = reverse_array(original_array)
+
+print("Original array :", original_array)
+print("Reversed array :", reversed_array)
