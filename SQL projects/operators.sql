51bb0821cf1990a7e6ef33958658b54305e47d69
arghya2300 <arghya.sarkar.bd@gmail.com>
2025-07-01 12:01:26 +0100

    create table

--- SQL projects/operators.sql
+++ SQL projects/operators.sql
@@ -1,22 +1,6 @@
-CREATE TABLE employees (
+CREATE TABLE customers (
     id INT,
     name VARCHAR(50),
-    department VARCHAR(50),
-    salary INT
+    city VARCHAR(50),
+    grade INT
 );
-
-
-INSERT INTO employees (id, name, department, salary) VALUES
-(1, 'Alice', 'HR', 50000),
-(2, 'Bob', 'Finance', 60000),
-(3, 'Charlie', 'IT', 70000),
-(4, 'David', 'Finance', 55000),
-(5, 'Eva', 'IT', 72000);
-
-SELECT * FROM employees;
-
-SELECT * FROM employees
-WHERE department = 'Finance';
-
-SELECT * FROM employees
-WHERE salary > 60000;
