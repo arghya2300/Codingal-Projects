388942ee4e4b16ed427d1528766aadc3a890312c
arghy2300 <arghya.sarkar.bd@gmail.com>
2025-10-05 14:11:58 +0100

    Other stuffs

---
+++ portfolio project backup2/portfolio project (1)/demo (1).js
@@ -0,0 +1,32 @@
+document.getElementById("hideShow1").style.display = "none";
+
+document.getElementById("hideShow2").style.display = "none";
+
+document.getElementById("hideShow3").style.display = "none";
+
+
+
+function toggle1() {
+    var x = document.getElementById("hideShow1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function toggle2() {
+    var x = document.getElementById("hideShow2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function toggle3() {
+    var x = document.getElementById("hideShow3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
