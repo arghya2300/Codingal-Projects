514c85724a2a9b7f24d2a31ed27526a0897db9be
arghy2300 <arghya.sarkar.bd@gmail.com>
2026-01-11 10:57:21 +0000

    conditional

---
+++ Basics of Probabilty/conditionak-HW.py
@@ -0,0 +1,11 @@
+
+red = 1
+blue = 6
+white = 3
+
+total = red + blue + white
+
+p_second_white_given_first_white = white / total
+
+print("P(Second ball is white | First ball is white) =", p_second_white_given_first_white)
+print("As a fraction:", f"{white}/{total}")
